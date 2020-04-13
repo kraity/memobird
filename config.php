4b0398d3612ba70c98f6d40bc798bec52e363903
权那他 <kraits@qq.com>
2020-04-13 19:17:06 +0800

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!defined('memobird')) exit('Access Violation');
-class config{
-    public static $ak = ''; //access key
-    public static $memobirdID = '';//设备编码
-    public static $str = '';//用户标识
-}
